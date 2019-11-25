a8d55c12a5434bc60523953ea87349072e2622c2
Theo Lampert <theolampert@gmail.com>
2019-11-25 19:36:35 +0100

    refactor: linter, ts, test. ci

---
+++ app/components/TabBarIcon.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+import Colors from "../constants/Colors";
+
+interface Props {
+  name: string,
+  focused: boolean,
+}
+
+export default function TabBarIcon(props: Props) {
+  return (
+    <Ionicons
+      name={props.name}
+      size={26}
+      style={{ marginBottom: -3 }}
+      color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+    />
+  );
+}
