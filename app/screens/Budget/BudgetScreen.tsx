6e2473dd9136e413319f26a4fead8644411436a8
Pierre Bresson <PierreBresson@users.noreply.github.com>
2019-11-25 21:50:24 +0100

    update app.json + comment error

--- app/screens/Budget/BudgetScreen.tsx
+++ app/screens/Budget/BudgetScreen.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ScrollView, StyleSheet } from "react-native";
 
 export default function BudgetScreen() {
-  throw new Error("My first Sentry error!");
+  // throw new Error("My first Sentry error!");
 
   return <ScrollView style={styles.container}></ScrollView>;
 }
