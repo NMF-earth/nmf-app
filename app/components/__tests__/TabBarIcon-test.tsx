f0993ad3c79e0e9857e3cba040812c42a273412b
Theo Lampert <theolampert@gmail.com>
2019-11-27 22:37:50 +0100

    refactor: rename to ts and add tests

---
+++ app/components/__tests__/TabBarIcon-test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import TabBarIcon from "../TabBarIcon";
+
+
+it("renders correctly", () => {
+  const tree = renderer.create(<TabBarIcon focused={false} name="ios-information-circle"/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
