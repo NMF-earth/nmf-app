76ce39f87216368de470994001006199de9266a6
Theo Lampert <theolampert@gmail.com>
2019-12-09 15:01:29 +0100

    feat: add in TabbedView component

---
+++ app/components/TabbedView/TabbedView.tsx
@@ -0,0 +1,68 @@
+import React, { ReactElement } from "react";
+import { Button, View } from "react-native";
+
+import styles from "./TabbedView.styles";
+import colors from "../../style/colors";
+
+interface TabbedViewItem {
+  title: string;
+  component: ReactElement;
+}
+
+interface Props {
+  items: TabbedViewItem[];
+}
+
+interface State {
+  selectedTab: number;
+}
+
+export default class TabbedView extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      selectedTab: 0,
+    }
+
+    this.renderTabItem = this.renderTabItem.bind(this);
+  }
+
+  handleTabPress(index: number): void {
+    this.setState({
+      selectedTab: index
+    })
+  }
+
+  renderTabItem({ title }, index: number) {
+    const { selectedTab } = this.state;
+    const isSelected = (index === selectedTab);
+    const buttonColor = isSelected ? colors.linkGreen : colors.gray;
+    const tabStyles = isSelected ? styles.tabItemSelected : styles.tabItem;
+    return (
+      <View 
+      key={index} 
+      style={tabStyles}>
+        <Button 
+          color={buttonColor}
+          onPress={() => this.handleTabPress(index)} 
+          title={title} />
+      </View>
+    );
+  }
+
+  render(): ReactElement {
+    const { items } = this.props;
+    const { selectedTab } = this.state;
+    return (
+      <View>
+        <View style={styles.tabContainer}>
+        {items.map(this.renderTabItem)}
+        </View>
+        {items[selectedTab].component}
+      </View>
+    );
+  }
+}
+
---
+++ app/components/TabbedView/__tests__/TabbedView.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import TabbedView from "../";
+import { Text } from "react-native";
+
+it("renders correctly TabbedView", () => {
+  const items = [
+    {
+      title: "Hello Tab 1",
+      component: <Text>Some details</Text>
+    },
+    {
+      title: "Hello Tab 1",
+      component: <Text>Some details</Text>
+    }
+  ];
+  const tree = renderer.create(<TabbedView items={items} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
