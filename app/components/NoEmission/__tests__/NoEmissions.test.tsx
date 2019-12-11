2b4634787638df49accc57ae8a0c14f41259b3da
Pierre Bresson <PierreBresson@users.noreply.github.com>
2019-12-11 18:50:42 +0100

    fix tests

--- app/components/NoEmission/__tests__/NoEmissions.test.tsx
+++ app/components/NoEmission/__tests__/NoEmissions.test.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import renderer from "react-test-renderer";
 import NoEmission from "../";
 
+jest.unmock("../");
+
 it("renders correctly NoEmission", () => {
   const tree = renderer.create(<NoEmission addEmission={() => {}} />).toJSON();
   expect(tree).toMatchSnapshot();
