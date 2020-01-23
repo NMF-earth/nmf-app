e96c31bf9d76392d6ce7c06698e5c88ad97b829a
Theo Lampert <theolampert@gmail.com>
2020-01-23 20:35:47 +0100

    feat: add redux-persist

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,10 +1,19 @@
 import { configureStore, Action } from "@reduxjs/toolkit";
 import { ThunkAction } from "redux-thunk";
+import { persistReducer } from "redux-persist"
+import { AsyncStorage } from "react-native";
 
 import rootReducer, { RootState } from "./rootReducer";
 
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer
+  reducer: persistedReducer
 });
 
 export type AppDispatch = typeof store.dispatch;
