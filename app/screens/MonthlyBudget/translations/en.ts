87c8c754ce20bdda1b9cb516316631f2030b2d58
Pierre Bresson <PierreBresson@users.noreply.github.com>
2020-02-01 20:58:25 +0100

    fix slider and add example to monthly budget

--- app/screens/MonthlyBudget/translations/en.ts
+++ app/screens/MonthlyBudget/translations/en.ts
@@ -1,5 +1,16 @@
 export default {
   MONTHLY_BUDGET_TITLE: "Monthly Budget",
   MONTHLY_BUDGET_MY_MONTHLY_CARBON_BUDGET: "My carbon monthly budget : ",
-  MONTHLY_BUDGET_SAVE: "Save"
+  MONTHLY_BUDGET_SAVE: "Save",
+  MONTHLY_BUDGET_CARBON_EMISSIONS_WORLD:
+    "CO2 emissions per capita per month per month ",
+  MONTHLY_BUDGET_LUXEMBOURG: "Luxembourg : 3.5 tons",
+  MONTHLY_BUDGET_UNITED_STATES: "United States : 1,5 tons",
+  MONTHLY_BUDGET_JAPAN: "Japan : 900 kg",
+  MONTHLY_BUDGET_SWEDEN: "Sweden : 595 kg",
+  MONTHLY_BUDGET_FRANCE: "France : 575 kg",
+  MONTHLY_BUDGET_CHINA: "China : 522 kg",
+  MONTHLY_BUDGET_BRAZIL: "Brazil : 208 kg",
+  MONTHLY_BUDGET_INDIA: "India : 139 kg",
+  MONTHLY_BUDGET_ETHIOPIA: "Ethiopia : 8.3 kg"
 };
