bd5dd2700d0075cd89a2d4f53a7a2d68848e9ce4
Pierre Bresson <PierreBresson@users.noreply.github.com>
2020-03-03 19:17:54 +0100

    fix fr translation

--- app/screens/Intro/translations/fr.ts
+++ app/screens/Intro/translations/fr.ts
@@ -1,9 +1,9 @@
 export default {
   INTRO_SCREEN_WELCOME: "Bienvenue sur NMF.earth",
   INTRO_SCREEN_PRELUDE:
-    "Are you ready to reduce your waste and transition to a sustainable life?",
+    "Etes vous pret a réduire vos déchets and adopter un mode de vie plus sain?",
   INTRO_SCREEN_CONTINUE_AND_ACCEPT_TERMS_OF_USE:
     "En continuant vous acceptez nos conditions d'utilisations.",
   INTRO_SCREEN_TERMS_OF_USE: "Lire les conditions d'utilisations",
-  INTRO_SCREEN_I_AGREE: "I agree"
+  INTRO_SCREEN_I_AGREE: "J'accepte"
 };
