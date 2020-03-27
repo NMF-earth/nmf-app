9f878b046a6288aa087349daf1e21e4d43a39614
Theo Lampert <mail@theo.sh>
2020-03-27 14:40:09 +0100

    fix: add missing file to ci generation script

--- scripts/generate-ci-config.js
+++ scripts/generate-ci-config.js
@@ -2,14 +2,26 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
 const path = require("path");
 const fs = require("fs");
-const appJsonTemplate = require(`${path.join(process.env.PWD, "app.example.json")}`);
+
+const pwd = process.env.PWD
+const appJsonTemplate = require(`${path.join(pwd, "app.example.json")}`);
 
 appJsonTemplate.expo.hooks.postPublish
   .find(item => item.file == "sentry-expo/upload-sourcemaps").config.authToken = process.env.SENTRY_AUTH_TOKEN
 
+const secrets = `export default {
+    dsn: "${process.env.SENTRY_DSN}"
+  }`;
+
 fs.writeFile(
-  `${path.join(process.env.PWD, "app.json")}`, 
+  `${path.join(pwd, "app.json")}`, 
   JSON.stringify(appJsonTemplate, null, 2), 
   () => { console.log("app.json file written") }
+);
+
+fs.writeFile(
+  `${path.join(pwd, "secret.ts")}`, 
+  secrets,
+  () => { console.log("secrets.ts file written") }
 );
 
