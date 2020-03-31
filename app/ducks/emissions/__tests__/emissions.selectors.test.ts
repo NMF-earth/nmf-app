772fe434df0dd5b807c27bb9acf76fc1fca46ad3
Pierre Bresson <PierreBresson@users.noreply.github.com>
2020-03-31 13:37:37 +0200

    add tests

--- app/ducks/emissions/__tests__/emissions.selectors.test.ts
+++ app/ducks/emissions/__tests__/emissions.selectors.test.ts
@@ -1,27 +1,50 @@
 import emissions from "../";
 import { Emission as EmissionType, EmissionEnum } from "../../../interfaces";
-import { FoodEnum } from "carbon-footprint";
+import { FoodEnum, TransportEnum } from "carbon-footprint";
 
 let state;
 
-const emissionToMitigate: EmissionType = {
-  id: "123",
+const emissionFood: EmissionType = {
+  id: "1",
   creationDate: "2020-01-26T11:04:55.334Z",
-  emissionModelType: FoodEnum.beans,
+  emissionModelType: FoodEnum.redMeat,
   emissionType: EmissionEnum.food,
   isMitigated: false,
   value: 10
 };
 
+const emissionTransport: EmissionType = {
+  id: "2",
+  creationDate: "2020-01-26T11:04:55.334Z",
+  emissionModelType: TransportEnum.bus,
+  emissionType: EmissionEnum.transport,
+  isMitigated: false,
+  value: 13
+};
+
+const emissionCustom: EmissionType = {
+  id: "3",
+  creationDate: "2020-01-26T11:04:55.334Z",
+  emissionModelType: "custom",
+  emissionType: EmissionEnum.custom,
+  isMitigated: false,
+  value: 100
+};
+
 const emissionMitigated: EmissionType = {
-  ...emissionToMitigate,
+  ...emissionFood,
   isMitigated: true
 };
 
 describe("if there are emissions", () => {
   beforeEach(() => {
     state = {
-      [emissions.namespace]: [emissionMitigated, emissionToMitigate]
+      [emissions.namespace]: [
+        emissionMitigated,
+        emissionFood,
+        emissionCustom,
+        emissionTransport
+      ]
     };
   });
 
@@ -32,7 +55,25 @@ describe("if there are emissions", () => {
 
   test("`getEmissionsToMitigate` should return mitigated emissions", () =>
     expect(emissions.selectors.getEmissionsToMitigate(state)).toEqual([
-      emissionToMitigate
+      emissionFood,
+      emissionCustom,
+      emissionTransport
+    ]));
+
+  test("`getCustomEmissions` should return mitigated emissions", () =>
+    expect(emissions.selectors.getCustomEmissions(state)).toEqual([
+      emissionCustom
+    ]));
+
+  test("`getFoodEmissions` should return mitigated emissions", () =>
+    expect(emissions.selectors.getFoodEmissions(state)).toEqual([
+      emissionMitigated,
+      emissionFood
+    ]));
+
+  test("`getTransportEmissions` should return mitigated emissions", () =>
+    expect(emissions.selectors.getTransportEmissions(state)).toEqual([
+      emissionTransport
     ]));
 });
 
@@ -43,9 +84,18 @@ describe("if there are no emissions", () => {
     };
   });
 
-  test("`getEmissionsMitigated` should return mitigated emissions", () =>
+  test("`getEmissionsMitigated` should return mitigated no emission", () =>
     expect(emissions.selectors.getEmissionsMitigated(state)).toEqual([]));
 
-  test("`getEmissionsToMitigate` should return mitigated emissions", () =>
+  test("`getEmissionsToMitigate` should return mitigated no emission", () =>
     expect(emissions.selectors.getEmissionsToMitigate(state)).toEqual([]));
+
+  test("`getCustomEmissions` should return mitigated no emission", () =>
+    expect(emissions.selectors.getCustomEmissions(state)).toEqual([]));
+
+  test("`getFoodEmissions` should return mitigated no emission", () =>
+    expect(emissions.selectors.getFoodEmissions(state)).toEqual([]));
+
+  test("`getTransportEmissions` should return mitigated no emission", () =>
+    expect(emissions.selectors.getTransportEmissions(state)).toEqual([]));
 });
