d3c3355adc51fe3420fe1da7e2dcccf16b91c1dc
Pierre Bresson <PierreBresson@users.noreply.github.com>
2020-04-02 19:16:39 +0200

    fix close modal not working coming soon screen

--- app/screens/ComingSoon/ComingSoonScreen.navigationOptions.tsx
+++ app/screens/ComingSoon/ComingSoonScreen.navigationOptions.tsx
@@ -5,25 +5,23 @@ import { Colors } from "../../style";
 import { PADDING_HORIZONTAL } from "../../constants/Layout";
 import { navigate } from "../../navigation";
 
-const navigationOptions = ({ navigation }) => {
-  const navigator = navigate(navigation);
-
-  return {
-    headerStyle: {
-      borderBottomWidth: 0,
-      marginRight: PADDING_HORIZONTAL
-    },
-    header: props => <Header {...props} />,
-    headerLeft: null,
-    headerRight: (
-      <Ionicons
-        name="md-close"
-        size={32}
-        color={Colors.darkLink}
-        onPress={navigator.goBack}
-      />
-    )
-  };
-};
+const navigationOptions = ({ navigation }) => ({
+  headerStyle: {
+    borderBottomWidth: 0,
+    marginRight: PADDING_HORIZONTAL
+  },
+  header: props => <Header {...props} />,
+  headerLeft: null,
+  headerRight: (
+    <Ionicons
+      name="md-close"
+      size={32}
+      color={Colors.darkLink}
+      onPress={() => {
+        navigate(navigation).goBack();
+      }}
+    />
+  )
+});
 
 export default navigationOptions;
