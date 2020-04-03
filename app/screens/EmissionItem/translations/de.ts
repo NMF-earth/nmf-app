eb2cb5af6ccf8f7708922128a98dbc9911d30e7c
Lucien Blunk-Lallet <lucien.blunk.lallet@gmail.com>
2020-04-03 11:35:36 +0200

    chore: convert all ts translation files to json

--- app/screens/EmissionItem/translations/de.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  EMISSION_ITEM_TITTLE: "Emission",
-  EMISSION_ITEM_TYPE: "Type",
-  EMISSION_ITEM_QUANTITY: "Quantity",
-  EMISSION_ITEM_PRICE: "Price",
-  EMISSION_ITEM_DATE: "Date",
-  EMISSION_ITEM_DELETE_EMISSION: "Delete emission",
-  EMISSION_ITEM_NAME: "Name"
-};
--- app/screens/EmissionItem/translations/fr.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  EMISSION_ITEM_TITLE: "Emission",
-  EMISSION_ITEM_TYPE: "Type",
-  EMISSION_ITEM_QUANTITY: "Quantité",
-  EMISSION_ITEM_PRICE: "Prix",
-  EMISSION_ITEM_DATE: "Date",
-  EMISSION_ITEM_DELETE_EMISSION: "Supprimer émission",
-  EMISSION_ITEM_NAME: "Nom"
-};
