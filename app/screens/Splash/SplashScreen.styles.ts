4a64b65f37e49cefdab127893f50e87af26f771a
devanshujain95 <devanshujain95@gmail.com>
2020-06-14 11:32:45 +0530

    [fix/50] Animated Splash Screens. Added SplashScreen component to show fadein animation at startup. Removed splash key from app.json.

---
+++ app/screens/Splash/SplashScreen.styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  view: { flex: 1, alignItems: "center", justifyContent: "center" },
+  animationImage: {
+    height: 450,
+    width: 450
+  }
+});
