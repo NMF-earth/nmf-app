57c44a7f85c06171f434cdf491f98b196978ddaf
Laura Meckley <laura.noelle.meckley@gmail.com>
2020-10-04 18:53:23 -0600

    Add babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,18 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            "components": "./app/components",
+            "utils": "./app/utils",
+          }
+        }
+      ]
+    ]
   };
 };
