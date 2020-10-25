4b214e1d72ebbfe851afe48708c45b47ab938025
Alireza Ghamkhar <mra.ghamkhar@gmail.com>
2020-10-25 23:12:12 +0330

    Add Notification page for receiving scheduled notifications

---
+++ app/screens/Notifications/NotificationsScreen.tsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useState } from "react";
+import { View, Switch } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Text } from "../../components";
+import { t } from "../../utils";
+import { userPreferences } from "../../ducks";
+import navigationOptions from "./NotificationsScreen.navigationOptions";
+import styles from "./NotificationsScreen.styles";
+
+const MyLocationScreen = () => {
+  const dispatch = useDispatch();
+  const [activated, setActivated] = useState(
+    useSelector(userPreferences.selectors.getActivateNotifications)
+  );
+
+  const onValueChange = useCallback(
+    async (value: boolean) => {
+      await Notifications.requestPermissionsAsync();
+      if (value) {
+        await Notifications.scheduleNotificationAsync({
+          content: {
+            title: t("NOTIFICATIONS_SCREEN_NOTIFICATION_TITLE"),
+            body: t("NOTIFICATIONS_SCREEN_NOTIFICATION_BODY"),
+          },
+          trigger: {
+            weekday: 1,
+            hour: 21,
+            minute: 0,
+            repeats: true,
+          },
+        });
+      } else {
+        await Notifications.cancelAllScheduledNotificationsAsync();
+      }
+      setActivated(value);
+      dispatch(userPreferences.actions.toggleNotifications(value));
+    },
+    [dispatch]
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text.Primary style={styles.intro}>
+        {t("NOTIFICATIONS_SCREEN_INTRO")}
+      </Text.Primary>
+      <View style={styles.rowContainer}>
+        <Text.Secondary bold>
+          {t("NOTIFICATIONS_SCREEN_ACTIVATED")}
+        </Text.Secondary>
+        <Switch value={activated} onValueChange={onValueChange} />
+      </View>
+    </View>
+  );
+};
+
+MyLocationScreen.navigationOptions = navigationOptions;
+
+export default MyLocationScreen;
