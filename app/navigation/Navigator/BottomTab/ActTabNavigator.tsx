63e7c06e0f8345ec10fce06669c6667ae656dc7d
Nightr0ck <leequan2000@gmail.com>
2021-07-26 21:28:40 +0800

    update top tab bar in ActTabNavigator

--- app/navigation/Navigator/BottomTab/ActTabNavigator.tsx
+++ app/navigation/Navigator/BottomTab/ActTabNavigator.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TouchableOpacity, View } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 import { Text } from "components";
@@ -12,20 +13,51 @@ const Tab = createMaterialTopTabNavigator();
 
 const tabs = Object.keys(GuideCategory);
 
+/* TODO: write tests for TopTabBar function */
+function TopTabBar({ state, navigation }) {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        paddingTop: 10,
+        paddingLeft: 10,
+        backgroundColor: Colors.white,
+      }}
+    >
+      {tabs.map((tab, index) => {
+        const label = tab;
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          if (!isFocused) {
+            navigation.navigate(tab);
+          }
+        };
+
+        return (
+          <TouchableOpacity key={label} onPress={onPress}>
+            <Text.Secondary
+              style={{
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                borderRadius: 13,
+                fontFamily: Font.FontWeight.Bold,
+                textTransform: "capitalize",
+                color: isFocused ? Colors.black : Colors.grey40,
+                backgroundColor: isFocused ? Colors.green10 : Colors.white,
+              }}
+            >
+              {label}
+            </Text.Secondary>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
 const ActTabNavigator = () => (
-  <Tab.Navigator
-    tabBarOptions={{
-      scrollEnabled: true,
-      activeTintColor: Colors.green50,
-      inactiveTintColor: Colors.grey40,
-      indicatorStyle: { backgroundColor: Colors.green50 },
-      labelStyle: {
-        fontSize: Font.FontSize.Secondary,
-        fontFamily: Font.FontWeight.Bold,
-      },
-      style: { backgroundColor: Colors.white },
-    }}
-  >
+  <Tab.Navigator tabBar={(props) => <TopTabBar {...props} />}>
     {tabs.map((tab) => (
       <Tab.Screen key={tab} name={tab} component={ActScreen} options={{ tabBarLabel: tab }} />
     ))}
