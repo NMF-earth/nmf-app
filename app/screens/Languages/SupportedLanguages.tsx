9f87c1b942fc6857996e3440020215df99c09962
adrianxx321 <lianghan321@gmail.com>
2021-07-28 22:47:41 +0800

    Extract supported languages list for future convenience

---
+++ app/screens/Languages/SupportedLanguages.tsx
@@ -0,0 +1,13 @@
+const SupportedLanguages = {
+  // Key-value pairs
+  // State the name of the language in its own language :)
+  en: "English",
+  fr: "Français",
+  de: "Deutsche",
+  sv: "Slovenščina",
+  ru: "Pусский",
+  pt: "Português",
+  pl: "Polskie",
+};
+
+export default SupportedLanguages;
