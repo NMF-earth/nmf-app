a6c30741b058ebd2fa8b356d731dfbb05d0cf570
adrianxx321 <lianghan321@gmail.com>
2021-08-04 18:05:59 +0800

    Modify language screen entry so that it won't appear in production mode

--- app/screens/Settings/SettingsScreen.tsx
+++ app/screens/Settings/SettingsScreen.tsx
@@ -33,10 +33,6 @@ const SettingsScreen = (props) => {
       onPress: navigator.openMyLocation,
     },
     {
-      title: t("SETTINGS_SCREEN_LANGUAGES"),
-      onPress: navigator.openLanguages,
-    },
-    {
       title: t("SETTINGS_SCREEN_SUPPORT_US"),
       onPress: navigator.openSupportUs,
     },
@@ -64,6 +60,14 @@ const SettingsScreen = (props) => {
       onPress: () => WebBrowser.openBrowserAsync("http://nmf.earth/terms-of-use.pdf"),
     },
   ];
+
+  if (__DEV__) {
+    rowItems.push({
+      title: t("SETTINGS_SCREEN_LANGUAGES"),
+      onPress: navigator.openLanguages,
+    });
+  }
+
   const [steps, setSteps] = useState(0);
 
   return (
