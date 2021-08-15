56b5003fd782f09cdbcf3feddeba1a530d19a685
Alireza Ghamkhar <mra.ghamkhar@gmail.com>
2021-08-15 22:00:51 +0430

    Fix: mock date on emission item screen to prevent unwanted snapshot updates

--- app/screens/EmissionItem/__tests__/EmissionItemScreen.test.tsx
+++ app/screens/EmissionItem/__tests__/EmissionItemScreen.test.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import MockDate from "mockdate";
 import { create } from "react-test-renderer";
 
 import EmissionItemScreen from "../EmissionItemScreen";
 
 it("EmissionsScreen renders correctly", () => {
+  const nowString = "2021-02-21T21:21:21";
+  MockDate.set(nowString);
   const tree = create(<EmissionItemScreen />).toJSON();
+  MockDate.reset();
   expect(tree).toMatchSnapshot();
 });
