9e1e2d486ab09f25d638aa881d72d0b0317fcaac
Sunki Baek <sunki.baek@gmail.com>
2021-10-10 00:44:21 -0700

    Add faq screen

--- app/screens/Faq/__tests__/FaqScreen.test.tsx
+++ app/screens/Faq/__tests__/FaqScreen.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable  @typescript-eslint/explicit-module-boundary-types */
 import React from "react";
 import { create } from "react-test-renderer";
 
@@ -6,5 +5,6 @@ import FaqScreen from "../FaqScreen";
 
 it("FaqScreen renders correctly", () => {
   const tree = create(<FaqScreen />).toJSON();
+
   expect(tree).toMatchSnapshot();
 });
