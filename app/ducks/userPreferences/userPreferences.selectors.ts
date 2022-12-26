2df6b405afb389384f278487dea4e0d52327b1fc
Tom Latham <tlatham54@gmail.com>
2022-12-26 15:29:10 -0800

    removed unused import

--- app/ducks/userPreferences/userPreferences.selectors.ts
+++ app/ducks/userPreferences/userPreferences.selectors.ts
@@ -1,5 +1,5 @@
 /* eslint-disable  @typescript-eslint/explicit-module-boundary-types */
-import { path, pathOr } from "ramda";
+import { pathOr } from "ramda";
 import { ElectricityType } from "carbon-footprint";
 
 import { currentLanguage } from "utils";
