656aa2e88f25efc58effefe6fd59d254abdb5616
Pierre Bresson <pierre@bresson.io>
2023-03-17 11:22:34 +0100

    remove unseless import

--- jest/mock/node-modules/index.js
+++ jest/mock/node-modules/index.js
@@ -1,5 +1,3 @@
-import { PermissionStatus } from "expo-barcode-scanner";
-
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 jest.mock("react-native-svg", () => {
