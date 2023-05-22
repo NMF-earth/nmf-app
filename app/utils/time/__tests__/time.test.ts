63b566191046b2f6989da183d4226592b08328ec
JohanRochet <91260794+JohanRochet@users.noreply.github.com>
2023-05-22 18:33:21 +0200

    mutation testing on time.ts

--- app/utils/time/__tests__/time.test.ts
+++ app/utils/time/__tests__/time.test.ts
@@ -27,28 +27,50 @@ it("if incorrect input, convertMinutesToHoursAnMinutes should return default val
   expect(time.convertMinutesToHoursAnMinutes(undefined)).toEqual(defaultValue);
 });
 
-it("getEarlierTime should return the earlier date object when 2 dates are compared", () => {
+describe("getEarlierTime should return the earlier date object when 2 dates are compared", () => {
   // arrange
   const date1 = new Date();
   const date2 = new Date();
   date2.setTime(date2.getTime() + (1 + Math.floor(Math.random() * 100000))); // set the time to some future time
 
+  it("date1 is earlier than date2", () => {
+    // act
+    const earlierDate = time.getEarlierDate(date1, date2);
+
+    // assert
+    expect(earlierDate.getTime()).toEqual(date1.getTime());
+  });
+
+  it("date2 is earlier than date1", () => {
   // act
-  const earlierDate = time.getEarlierDate(date1, date2);
+  const earlierDate = time.getEarlierDate(date2, date1);
 
   // assert
   expect(earlierDate.getTime()).toEqual(date1.getTime());
+  });
+
 });
 
-it("getLaterTime should return the later date object when 2 dates are compared", () => {
+describe("getLaterTime should return the later date object when 2 dates are compared", () => {
   // arrange
   const date1 = new Date();
   const date2 = new Date();
   date2.setTime(date2.getTime() + (1 + Math.floor(Math.random() * 100000))); // set the time to some future time
 
+  it("date1 is later than date2", () => {
+    // act
+    const laterDate = time.getLaterDate(date2, date1);
+
+    // assert
+    expect(laterDate.getTime()).toEqual(date2.getTime());
+  });
+
+  it("date2 is later than date1", () => {
   // act
   const laterDate = time.getLaterDate(date1, date2);
 
   // assert
   expect(laterDate.getTime()).toEqual(date2.getTime());
+  });
+  
 });
