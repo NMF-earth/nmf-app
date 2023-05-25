7db0a29acb04a23d639597e1a277990ffc9b4510
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-05-25 17:27:34 +0500

    added primary colors

--- app/style/colors/Colors.ts
+++ app/style/colors/Colors.ts
@@ -15,4 +15,13 @@ export default {
   black: "#000000",
   black70: "#575757",
   primary10: "#E7F3EB",
+  primary20: "##CFE7D8",
+  primary30: "#b7dac4",
+  primary40: "#9fceb1",
+  primary50: "#87c29d",
+  primary60: "#6fb689",
+  primary70: "#57aa76",
+  primary80: "#57aa76",
+  primary90: "#27914f",
+  primary: "#0F853B",
 };
