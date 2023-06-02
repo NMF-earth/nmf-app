527177db9ad26beec9af970215ffda2cee02e0b0
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-06-02 16:36:42 +0500

    Updated FAQs

--- app/screens/Faq/FaqContent.ts
+++ app/screens/Faq/FaqContent.ts
@@ -2,24 +2,36 @@ import { t } from "utils";
 
 const content = [
   {
-    title: t("FAQ_SCREEN_ENTER_MANUALLY_QUESTION"),
-    description: t("FAQ_SCREEN_ENTER_MANUALLY_ANSWER"),
+    title: t("FAQ_SCREEN_STARTUP_QUESTION"),
+    description: t("FAQ_SCREEN_STARTUP_ANSWER"),
   },
   {
-    title: t("FAQ_SCREEN_ACCURACY_QUESTION"),
-    description: t("FAQ_SCREEN_ACCURACY_ANSWER"),
+    title: t("FAQ_SCREEN_DATA_QUESTION"),
+    description: t("FAQ_SCREEN_DATA_ANSWER"),
   },
   {
     title: t("FAQ_SCREEN_GOAL_QUESTION"),
     description: t("FAQ_SCREEN_GOAL_ANSWER"),
   },
   {
-    title: t("FAQ_SCREEN_STARTUP_QUESTION"),
-    description: t("FAQ_SCREEN_STARTUP_ANSWER"),
+    title: t("FAQ_SCREEN_DIFFERENT_QUESTION"),
+    description: t("FAQ_SCREEN_DIFFERENT_ANSWER"),
   },
   {
-    title: t("FAQ_SCREEN_BUSINESS_QUESTION"),
-    description: t("FAQ_SCREEN_BUSINESS_ANSWER"),
+    title: t("FAQ_SCREEN_GREEN_WASHING_QUESTION"),
+    description: t("FAQ_SCREEN_GREEN_WASHING_ANSWER"),
+  },
+  {
+    title: t("FAQ_SCREEN_NMF_QUESTION"),
+    description: t("FAQ_SCREEN_NMF_ANSWER"),
+  },
+  {
+    title: t("FAQ_SCREEN_ENTER_MANUALLY_QUESTION"),
+    description: t("FAQ_SCREEN_ENTER_MANUALLY_ANSWER"),
+  },
+  {
+    title: t("FAQ_SCREEN_VEGAN_QUESTION"),
+    description: t("FAQ_SCREEN_VEGAN_ANSWER"),
   },
 ];
 
