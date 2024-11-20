10ee0809f416b3f5cf35136a5e25a6dd2dac1b27
Jiří Podhorecký <trendspotter@users.noreply.github.com>
2024-11-20 23:15:35 +0100

    Update index.ts
    add cs

--- app/screens/Settings/translations/index.ts
+++ app/screens/Settings/translations/index.ts
@@ -1,6 +1,7 @@
 import en from "./en.json";
 import fr from "./fr.json";
 import de from "./de.json";
+import cs from "./cs.json";
 import sv from "./sv.json";
 import es from "./es.json";
 import pt from "./pt.json";
@@ -31,4 +32,4 @@ interface TranslationKeys {
   SETTINGS_SCREEN_LANGUAGES: string;
 }
 
-export { en, fr, de, sv, es, pt, da, ru, pl, zh, ms, it, ar, TranslationKeys };
+export { en, fr, de, cs, sv, es, pt, da, ru, pl, zh, ms, it, ar, TranslationKeys };
