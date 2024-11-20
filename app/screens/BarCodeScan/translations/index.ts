af76fed6d2bc616b05935fd0d9f19f1b5a20bd32
Jiří Podhorecký <trendspotter@users.noreply.github.com>
2024-11-20 22:16:03 +0100

    Update index.ts
    add cs

--- app/screens/BarCodeScan/translations/index.ts
+++ app/screens/BarCodeScan/translations/index.ts
@@ -1,6 +1,7 @@
 import en from "./en.json";
 import fr from "./fr.json";
 import de from "./de.json";
+import cs from "./cs.json";
 import sv from "./sv.json";
 import es from "./es.json";
 import pt from "./pt.json";
@@ -24,4 +25,4 @@ interface TranslationKeys {
   BAR_CODE_SCAN_SCREEN_NO_CARBON_DATA_MESSAGE: string;
 }
 
-export { en, fr, de, sv, es, pt, da, ru, pl, zh, ms, it, ar, TranslationKeys };
+export { en, fr, de, cs, sv, es, pt, da, ru, pl, zh, ms, it, ar, TranslationKeys };
