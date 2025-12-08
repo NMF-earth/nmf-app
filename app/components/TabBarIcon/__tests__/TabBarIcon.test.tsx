bcd25a1c35b839600486248a77b32673df2460a6
Harshal Bhavsar <hb2708@gmail.com>
2025-12-08 17:40:56 +0800

    test: update TabBarIcon test for Universal Test Renderer

--- app/components/TabBarIcon/__tests__/TabBarIcon.test.tsx
+++ app/components/TabBarIcon/__tests__/TabBarIcon.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import { render } from "@testing-library/react-native";
 
 import TabBarIcon from "../TabBarIcon";
 
 it("renders correctly", () => {
-  const tree = create(<TabBarIcon focused={false} name="ios-information-circle" />).toJSON();
+  const tree = render(<TabBarIcon focused={false} name="ios-information-circle" />).toJSON();
   expect(tree).toMatchSnapshot();
 });
