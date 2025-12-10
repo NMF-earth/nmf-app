9cfa1cb7a6325dbbb7760907f6031039cd10df5d
Harshal Bhavsar <hb2708@gmail.com>
2025-12-10 21:53:35 +0800

    feat: Add `testID` props to `RCTText` components across various screens and components, updating snapshots.

--- jest/mock/components/Text.mock.js
+++ jest/mock/components/Text.mock.js
@@ -2,12 +2,12 @@ import React from "react";
 
 // Universal Test Renderer requires text to be in RCTText elements
 export default {
-  Primary: (props) => React.createElement("RCTText", props, props.children),
-  Secondary: (props) => React.createElement("RCTText", props, props.children),
-  Tertiary: (props) => React.createElement("RCTText", props, props.children),
-  H1: (props) => React.createElement("RCTText", props, props.children),
-  H2: (props) => React.createElement("RCTText", props, props.children),
-  H3: (props) => React.createElement("RCTText", props, props.children),
-  Link: (props) => React.createElement("RCTText", props, props.children),
-  Header: (props) => React.createElement("RCTText", props, props.children),
+  Primary: (props) => React.createElement("RCTText", { ...props, testID: "Primary" }, props.children),
+  Secondary: (props) => React.createElement("RCTText", { ...props, testID: "Secondary" }, props.children),
+  Tertiary: (props) => React.createElement("RCTText", { ...props, testID: "Tertiary" }, props.children),
+  H1: (props) => React.createElement("RCTText", { ...props, testID: "H1" }, props.children),
+  H2: (props) => React.createElement("RCTText", { ...props, testID: "H2" }, props.children),
+  H3: (props) => React.createElement("RCTText", { ...props, testID: "H3" }, props.children),
+  Link: (props) => React.createElement("RCTText", { ...props, testID: "Link" }, props.children),
+  Header: (props) => React.createElement("RCTText", { ...props, testID: "Header" }, props.children),
 };
