6da322ed8648d5cd8333e22b18e53b79bcc5f460
Harshal Bhavsar <hb2708@gmail.com>
2025-12-13 17:46:35 +0800

    chore: fix lint warnings due to typescript version mismatch (#407)

--- app/utils/translations/localization.tsx
+++ app/utils/translations/localization.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable  @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable  @typescript-eslint/ban-types */
+
 /* eslint-disable  @typescript-eslint/ban-ts-comment */
 
 import React from "react";
