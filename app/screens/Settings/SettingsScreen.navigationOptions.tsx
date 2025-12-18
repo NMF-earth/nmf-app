d385bb95599d2a1bf6301e5be3a711d57f1ae27e
Harshal Bhavsar <hb2708@gmail.com>
2025-12-18 17:39:02 +0800

    feat(settings): implement large header support

--- app/screens/Settings/SettingsScreen.navigationOptions.tsx
+++ app/screens/Settings/SettingsScreen.navigationOptions.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { StackNavigationOptions } from "@react-navigation/stack";
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 
 import { Text } from "components";
-import { t } from "utils";
-import { Layout, ComponentsStyle } from "style";
+import { platform, t } from "utils";
+import { Font, Layout } from "style";
 
-const navigationOptions = (): StackNavigationOptions => ({
-  headerStyle: {
-    ...ComponentsStyle.header,
+const navigationOptions = (): NativeStackNavigationOptions => ({
+  title: t("SETTINGS_SCREEN_TITLE"),
+  headerLargeTitleEnabled: true,
+  headerTransparent: platform.isIOS,
+  headerBlurEffect: platform.isIOS26OrLater() ? undefined : "regular",
+  headerLargeTitleStyle: {
+    fontFamily: Font.FontWeight.Black,
   },
-  headerBackTitle: null,
-  headerTitle: () => <Text.H1 style={Layout.androidNavTitle}>{t("SETTINGS_SCREEN_TITLE")}</Text.H1>,
+  headerTitleStyle: {
+    fontFamily: Font.FontWeight.Black,
+  },
+  headerTitle:
+    platform.isAndroid
+      ? () => <Text.H1 style={Layout.androidNavTitle}>{t("SETTINGS_SCREEN_TITLE")}</Text.H1>
+      : undefined,
 });
 
 export default navigationOptions;
