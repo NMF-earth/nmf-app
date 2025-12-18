397e8a35407b7ca8d46e9e47d51fcf9c01389135
Harshal Bhavsar <hb2708@gmail.com>
2025-12-18 17:31:32 +0800

    refactor(navigation): migrate tab stacks to native stack and update types

--- app/navigation/Navigator/BottomTab/EmissionsNavigator.tsx
+++ app/navigation/Navigator/BottomTab/EmissionsNavigator.tsx
@@ -1,33 +1,33 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator, NativeStackNavigationOptions } from "@react-navigation/native-stack";
 
 import EmissionItemScreen from "../../../screens/EmissionItem";
 import EmissionsScreen from "../../../screens/Emissions";
 import MonthlyEmissionsScreen from "../../../screens/MonthlyEmissions";
 import RecurringEmissionsScreen from "../../../screens/RecurringEmissions";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const EmissionsNavigator = (): React.ReactElement => (
   <Stack.Navigator id="EmissionsStack">
     <Stack.Screen
       name="Emissions"
-      options={EmissionsScreen.navigationOptions}
+      options={EmissionsScreen.navigationOptions as NativeStackNavigationOptions}
       component={EmissionsScreen}
     />
     <Stack.Screen
       name="EmissionItem"
-      options={EmissionItemScreen.navigationOptions}
+      options={EmissionItemScreen.navigationOptions as NativeStackNavigationOptions}
       component={EmissionItemScreen}
     />
     <Stack.Screen
       name="MonthlyEmissions"
-      options={MonthlyEmissionsScreen.navigationOptions}
+      options={MonthlyEmissionsScreen.navigationOptions as NativeStackNavigationOptions}
       component={MonthlyEmissionsScreen}
     />
     <Stack.Screen
       name="RecurringEmissions"
-      options={RecurringEmissionsScreen.navigationOptions}
+      options={RecurringEmissionsScreen.navigationOptions as NativeStackNavigationOptions}
       component={RecurringEmissionsScreen}
     />
   </Stack.Navigator>
