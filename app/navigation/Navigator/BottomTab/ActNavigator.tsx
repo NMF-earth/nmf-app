88e8131d844848d14443c07394a6a63408e8519f
Harshal Bhavsar <hb2708@gmail.com>
2025-12-18 18:16:20 +0800

    refactor: Migrate ActNavigator and ActTabNavigator to use native stack navigation and update associated options.

--- app/navigation/Navigator/BottomTab/ActNavigator.tsx
+++ app/navigation/Navigator/BottomTab/ActNavigator.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { createStackNavigator, StackNavigationOptions } from "@react-navigation/stack";
+import { createNativeStackNavigator, NativeStackNavigationOptions } from "@react-navigation/native-stack";
 
 import ActTabNavigator from "./ActTabNavigator";
 import ActDetailScreen from "../../../screens/ActDetail";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const ActNavigator = (): React.ReactElement => (
   <Stack.Navigator id="ActStack">
     <Stack.Screen
       name="ActTabNavigator"
-      options={ActTabNavigator.navigationOptions as StackNavigationOptions}
+      options={ActTabNavigator.navigationOptions as NativeStackNavigationOptions}
       component={ActTabNavigator}
     />
     <Stack.Screen
       name="ActDetail"
       component={ActDetailScreen}
-      options={ActDetailScreen.navigationOptions as StackNavigationOptions}
+      options={ActDetailScreen.navigationOptions as NativeStackNavigationOptions}
     />
   </Stack.Navigator>
 );
--- app/navigation/Navigator/BottomTab/ActTabNavigator.tsx
+++ app/navigation/Navigator/BottomTab/ActTabNavigator.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { StackNavigationOptions } from "@react-navigation/stack";
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 
 import { Text } from "components";
-import { t } from "utils";
-import { ComponentsStyle, Colors, Font, Layout } from "style";
+import { platform, t } from "utils";
+import { Colors, Font, Layout } from "style";
 import { NavStatelessComponent } from "interfaces";
 
 import { GuideCategory } from "../../../types/guide";
@@ -83,13 +83,23 @@ const ActTabNavigator: NavStatelessComponent = () => (
   </Tab.Navigator>
 );
 
-const navigationOptions = (): StackNavigationOptions => ({
-  ...ComponentsStyle.transitionBetweenScreenPresets,
-  headerStyle: {
-    ...ComponentsStyle.header,
-  },
+const navigationOptions = (): NativeStackNavigationOptions => ({
+  title: t("ACT_SCREEN_TITLE"),
+  headerLargeTitleEnabled: false,
+  headerTransparent: false,
+  headerBlurEffect: "regular",
   headerBackButtonDisplayMode: "minimal",
-  headerTitle: () => <Text.H1 style={Layout.androidNavTitle}>{t("ACT_SCREEN_TITLE")}</Text.H1>,
+  headerTintColor: Colors.grey100,
+  headerLargeTitleStyle: {
+    fontFamily: Font.FontWeight.Black,
+  },
+  headerTitleStyle: {
+    fontFamily: Font.FontWeight.Black,
+  },
+  headerTitle:
+    platform.isAndroid
+      ? () => <Text.H1 style={Layout.androidNavTitle}>{t("ACT_SCREEN_TITLE")}</Text.H1>
+      : undefined,
 });
 
 ActTabNavigator.navigationOptions = navigationOptions();
