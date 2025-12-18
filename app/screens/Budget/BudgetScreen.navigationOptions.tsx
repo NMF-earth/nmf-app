24b6e98a348d2e1f4ab47fdfe5cdfb5cafbe07d1
Harshal Bhavsar <hb2708@gmail.com>
2025-12-18 17:36:15 +0800

    feat(budget): implement large header support

--- app/screens/Budget/BudgetScreen.navigationOptions.tsx
+++ app/screens/Budget/BudgetScreen.navigationOptions.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { StackNavigationOptions } from "@react-navigation/stack";
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 
+import { t, platform } from "utils";
 import { Text } from "components";
-import { t } from "utils";
-import { Layout, ComponentsStyle } from "style";
+import { Layout, Font } from "style";
 
-const navigationOptions = (): StackNavigationOptions => ({
-  headerStyle: {
-    ...ComponentsStyle.header,
+const navigationOptions: NativeStackNavigationOptions = {
+  title: t("BUDGET_SCREEN_TITLE"),
+  headerLargeTitleEnabled: true,
+  headerTransparent: platform.isIOS,
+  headerBlurEffect: platform.isIOS26OrLater() ? undefined : "regular",
+  headerLargeTitleStyle: {
+    fontFamily: Font.FontWeight.Black,
   },
-  headerBackTitle: null,
-  headerTitle: () => <Text.H1 style={Layout.androidNavTitle}>{t("BUDGET_SCREEN_TITLE")}</Text.H1>,
-});
+  headerTitleStyle: {
+    fontFamily: Font.FontWeight.Black,
+  },
+  headerTitle:
+    platform.isAndroid
+      ? () => <Text.H1 style={Layout.androidNavTitle}>{t("BUDGET_SCREEN_TITLE")}</Text.H1>
+      : undefined,
+};
 
 export default navigationOptions;
