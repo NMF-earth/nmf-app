cd530d4ad51de41559a0d6d58c53f63058213a46
Harshal Bhavsar <hb2708@gmail.com>
2025-12-20 12:46:48 +0800

    refactor: standardize navigation options for modal screens

--- app/screens/InfoModal/InfoModal.navigationOptions.tsx
+++ app/screens/InfoModal/InfoModal.navigationOptions.tsx
@@ -1,45 +1,24 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { StackNavigationOptions } from "@react-navigation/stack";
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 
-import { Layout } from "constant";
-import { Text } from "components";
 import { platform, t, ui } from "utils";
-import { Colors, ComponentsStyle } from "style";
+import { Colors, Font } from "style";
 import { navigate } from "navigation";
 
-const iconStyle = { paddingRight: Layout.PADDING_HORIZONTAL };
 
-const navigationOptionsAndroid = ({ route }): StackNavigationOptions => ({
-  ...ComponentsStyle.transitionBetweenScreenPresets,
-  headerStyle: {
-    ...ComponentsStyle.header,
-  },
-  headerTitle: () =>
-    route.params?.emissionModelType ? (
-      <Text.H1>{ui.getTranslationEmissionModelType(route.params.emissionModelType)}</Text.H1>
-    ) : (
-      <Text.H1>{t("INFO_MODAL_SCREEN_METHODOLOGY")}</Text.H1>
-    ),
-  headerRight: () => null,
-});
-
-const navigationOptionsIOS = ({ navigation, route }): StackNavigationOptions => ({
-  headerStyle: {
-    ...ComponentsStyle.header,
-    borderBottomWidth: 0,
-  },
-  headerTitle: () =>
-    route.params?.emissionModelType ? (
-      <Text.H1>{ui.getTranslationEmissionModelType(route.params.emissionModelType)}</Text.H1>
-    ) : (
-      <Text.H1>{t("INFO_MODAL_SCREEN_METHODOLOGY")}</Text.H1>
-    ),
+const navigationOptions = ({ navigation, route }): NativeStackNavigationOptions => ({
+  title: route.params?.emissionModelType
+    ? ui.getTranslationEmissionModelType(route.params.emissionModelType)
+    : t("INFO_MODAL_SCREEN_METHODOLOGY"),
+  headerTitleAlign: "center",
+  headerBackVisible: false,
   headerLeft: () => null,
+  headerTransparent: platform.isIOS,
+  headerBlurEffect: platform.isIOS26OrLater() ? undefined : "regular",
   headerRight: () => (
     <TouchableOpacity
-      style={iconStyle}
       onPress={() => {
         navigate(navigation).goBack();
       }}
@@ -47,8 +26,11 @@ const navigationOptionsIOS = ({ navigation, route }): StackNavigationOptions => 
       <Ionicons name="close" size={32} color={Colors.grey100} />
     </TouchableOpacity>
   ),
+  headerTitleStyle: {
+    fontFamily: Font.FontWeight.Bold,
+    fontSize: Font.FontSize.Header,
+  },
+  headerTintColor: Colors.grey100,
 });
-
-const navigationOptions = platform.isAndroid ? navigationOptionsAndroid : navigationOptionsIOS;
 
 export default navigationOptions;
--- app/screens/Periodicity/PeriodicityModal.navigationOptions.tsx
+++ app/screens/Periodicity/PeriodicityModal.navigationOptions.tsx
@@ -1,39 +1,30 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { StackNavigationOptions } from "@react-navigation/stack";
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 
-import { Layout } from "constant";
-import { Text } from "components";
 import { platform, t } from "utils";
-import { Colors, ComponentsStyle } from "style";
+import { Colors, Font } from "style";
 import { navigate } from "navigation";
 
-const iconStyle = { paddingRight: Layout.PADDING_HORIZONTAL };
 
-const navigationOptionsAndroid = (): StackNavigationOptions => ({
-  ...ComponentsStyle.transitionBetweenScreenPresets,
-  headerStyle: {
-    ...ComponentsStyle.header,
-  },
-  headerTitle: () => <Text.H1>{t("PERIODICITY_MODAL_SCREEN_TITLE")}</Text.H1>,
-  headerRight: () => null,
-});
-
-const navigationOptionsIOS = ({ navigation }): StackNavigationOptions => ({
-  headerStyle: {
-    ...ComponentsStyle.header,
-    borderBottomWidth: 0,
-  },
-  headerTitle: () => <Text.H1>{t("PERIODICITY_MODAL_SCREEN_TITLE")}</Text.H1>,
+const navigationOptions = ({ navigation }): NativeStackNavigationOptions => ({
+  title: t("PERIODICITY_MODAL_SCREEN_TITLE"),
+  headerTitleAlign: "center",
+  headerBackVisible: false,
   headerLeft: () => null,
+  headerTransparent: platform.isIOS,
+  headerBlurEffect: platform.isIOS26OrLater() ? undefined : "regular",
   headerRight: () => (
-    <TouchableOpacity style={iconStyle} onPress={() => navigate(navigation).goBack()}>
+    <TouchableOpacity onPress={() => navigate(navigation).goBack()}>
       <Ionicons name="close" size={32} color={Colors.grey100} />
     </TouchableOpacity>
   ),
+  headerTitleStyle: {
+    fontFamily: Font.FontWeight.Bold,
+    fontSize: Font.FontSize.Header,
+  },
+  headerTintColor: Colors.grey100,
 });
-
-const navigationOptions = platform.isAndroid ? navigationOptionsAndroid : navigationOptionsIOS;
 
 export default navigationOptions;
